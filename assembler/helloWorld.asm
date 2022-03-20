42ca3cc8223ab4b385aa807938ddc0aa0762409d
Frederic Wilhelm <frederic.wilhelm@gmx.net>
2022-03-20 17:44:53 +0100

    assembler/helloWorld.asm: Update

--- assembler/helloWorld.asm
+++ assembler/helloWorld.asm
@@ -1,17 +1,19 @@
-SECTION .data					; Data
+; Actual code
+section .text					; Code
+	global main
+
+; Init data or constants
+; - does not change at runtime
+section .data					; Data
 msg:	db "Hello, World", 10	; String to be printed and `10` indicating `\n`
 len:	equ $-msg				; string length: current mem-loc minus mem-loc of 'msg'
 
-SECTION .text					; Code
-global main						;
-
 main:
-	mov edx, len				; set length
-	mov ecx, msg				; set data
-	mov ebx, 1					; 1 = stdout
-	mov eax, 4					; output
+	mov edx, len				; set length (data.len)
+	mov ecx, msg				; set data (data.msg)
+	mov ebx, 1					; file descriptor (stdout)
+	mov eax, 4					; system call number (sys_write==4)
 	int 0x80					; call linux kernel (?)
 
-	mov ebx, 0					; normal exit
-	mov eax, 1					; exit program
+	mov eax, 1					; system call number (sys_exit==1)
 	int 0x80
